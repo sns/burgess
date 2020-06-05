4c09974a690dca0865eca4f2ee58515275ea840b
Sina <sina.it@gmail.com>
2020-06-04 22:38:00 -0400

    reorganizing files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-from models.item import Item
+from models.requests.inventory.item import Item
 import json
 import logging
 
